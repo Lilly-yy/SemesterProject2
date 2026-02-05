c5f7146545487be27e2022391c6ebd77a5030184
Lilly-yy <142634906+Lilly-yy@users.noreply.github.com>
2026-02-05 09:08:16 +0100

    Vite multi-page build config

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        browse: resolve(__dirname, "browse.html"),
+        listing: resolve(__dirname, "listing.html"),
+        login: resolve(__dirname, "login.html"),
+        register: resolve(__dirname, "register.html"),
+        profile: resolve(__dirname, "profile.html"),
+        createListing: resolve(__dirname, "create-listing.html"),
+      },
+    },
+  },
+});
